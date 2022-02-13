6de25b69f8b7346b5949f8d501ff775d0f0efd27
tormadsen <tormads@ntnu.no>
2022-02-13 17:22:36 +0100

    Changelog: -Added more css styling and html elements to login page

--- GroupUpProject/templates/GroupUp/login/login_script.js
+++ GroupUpProject/templates/GroupUp/login/login_script.js
@@ -4,11 +4,12 @@ function getId(id) {
 }
 
 function show_register_overlay() {
-    getId('login').style.display = 'none';
-    getId('register_user').style.display = 'block';
+    getId('login_module').style.display = 'none';
+    getId('register_module').style.display = 'block';
 }
 
 function show_login_overlay() {
-    getId('register_user').style.display = 'none';
-    getId('login').style.display = 'block';
+    getId('register_module').style.display = 'none';
+    getId('login_module').style.display = 'block';
 }
+
