a07aaa4a5c8e3a337963e5c14a4880b6b2b701d3
tormadsen <tormads@ntnu.no>
2022-02-14 16:14:35 +0100

    Changelog: -Continued structuring html, almost done. -Added more css, not done. -fixed javascript that did not work before, needs more script to get all functionality.

--- GroupUpProject/templates/GroupUp/groups/groups_script.js
+++ GroupUpProject/templates/GroupUp/groups/groups_script.js
@@ -1,14 +1,23 @@
 
+/* Variables */
+
+let activeGroup = null;
+const testList = ["group1", "group2", "group3",
+                      "group4", "group5", "group6",
+                      "group7", "group8", "group9",
+                      "group10", "group11", "group12"];
+
+const testMembers = ["member1", "member2", "member3", "member4", "member5"];
+
+/* Functions */
+
 function getId(id) {
     return document.getElementById(id);
 }
 
 function addGroupsToList() {
     /* Some function to get list of groups from database */
-    const testList = ["test1", "test2", "test3",
-                      "test4", "test5", "test6",
-                      "test7", "test8", "test9",
-                      "test10", "test11", "test12"];
+
 
     let element = document.getElementById("groups_list");
 
@@ -18,12 +27,12 @@ function addGroupsToList() {
         newGroupElement.setAttribute("id", i.toString());
         newGroupElement.setAttribute("onclick", "drawGroupPage(" + i + ")");
 
-        let groupPicture = document.createElement("img")
+        let groupPicture = document.createElement("img");
         groupPicture.setAttribute("class", "group_image");
         newGroupElement.appendChild(groupPicture);
 
         let groupTitle = document.createElement("p");
-        let title = document.createTextNode("title");
+        let title = document.createTextNode(testList[i]);
         groupTitle.setAttribute("class", "group_title");
         groupTitle.appendChild(title);
         newGroupElement.appendChild(groupTitle);
@@ -32,10 +41,34 @@ function addGroupsToList() {
     }
 }
 
+function changeGroupListElementColor(element) {
+    if (activeGroup !== null) {
+        activeGroup.classList.remove("active");
+    }
+    element.classList.add("active");
+    activeGroup = element;
+}
+
 function drawGroupPage(group_index) {
     let groupNr = group_index.valueOf();
     let groupElement = getId(group_index);
-    groupElement.getAttribute("background-color")
-    console.log(groupElement.getAttribute("background-color"));
-    groupElement.setAttribute("background-color", "blue");
+    changeGroupListElementColor(groupElement);
+    console.log(activeGroup);
+
+    getId("group_name").innerHTML = testList[groupNr];
+    let memberList = getId("group_members");
+    while (memberList.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+    for (i = 0; i < testMembers.length; i++) {
+        let groupMember = document.createElement("li");
+        let text = document.createTextNode(testMembers[i]);
+        groupMember.appendChild(text);
+        memberList.appendChild(groupMember);
+    }
+}
+
+function init() {
+    addGroupsToList();
+    drawGroupPage("0");
 }
