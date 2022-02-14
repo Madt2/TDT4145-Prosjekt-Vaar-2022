8d4644011930f1c6074429ccb0094bf5e4862a7f
askis <askis@stud.ntnu.no>
2022-02-14 21:07:25 +0100

    Added some models to models.py and migrated changes to DB. Not sure if models are 100% correctly setup yet.

--- GroupUpProject/GroupUp/models.py
+++ GroupUpProject/GroupUp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Interests(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    picture = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length= 50, primary_key= True)
+    description = models.TextField()
+    picture = models.ImageField()
+    average_age = models.IntegerField()
+    location = models.CharField(max_length= 30)
+    date = models.DateTimeField(default=timezone.now)
+    members = models.ManyToManyField(User, through="GroupMembers", blank=False)
+    interests = models.ManyToManyField(Interests)
+
+    def __str__(self):
+        return self.name
+
+class GroupMembers(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField()
+
+class GroupMatches(models.Model):
+    review = models.IntegerField()
+
+class Reports(models.Model):
+    report_type = models.CharField(max_length=20)
+    description = models.TextField()
+    reported_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+
+
+
