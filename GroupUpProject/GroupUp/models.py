0e595c521f0c74145261ad62b8ba250379841483
Emil <epa1313@gmail.com>
2022-02-18 11:59:21 +0100

    added database settings, updated models and added gitignore

--- GroupUpProject/GroupUp/models.py
+++ GroupUpProject/GroupUp/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uniqueID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique = True, editable=False)
     first_name = models.CharField(default="", max_length=30)
     last_name = models.CharField(default="", max_length=30)
     email = models.EmailField("User's e-mail")
@@ -17,13 +16,12 @@ class Profile(models.Model):
     def age(self):
         today = date.today()
         born = self.date_of_birth
-        return today.year-born.year-((today.month, today.day)<(born.month, born.day))
-    
+        return today.year-born.year-((today.month, today.day) < (born.month, born.day))
+
     @property
     def name(self):
         return self.first_name + ' ' + self.last_name
 
-    
     def __meta__(self):
         db_table = 'GroupUp_profile'
 
@@ -34,28 +32,30 @@ class Profile(models.Model):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
 class Interest(models.Model):
-    interestID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField("NameOfInterest", max_length=30)
 
     def __str__(self):
         return self.name
 
+
 class Group(models.Model):
-    groupID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique = True, editable=False)
     name = models.CharField('Group Name', max_length=120)
-    group_leader = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    group_leader = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField("Description", blank=True)
-    members = models.ManyToManyField(Profile, through="MemberOfGroup", blank=True)
+    members = models.ManyToManyField(
+        Profile, through="MemberOfGroup", blank=True)
     location = models.CharField("Location", max_length=30)
-    interests = models.ManyToManyField(Interest, through="")
+    interests = models.ManyToManyField(Interest, through="GroupHasInterest")
     image = models.ImageField("GroupImage")
 
     objects = models.Manager()
 
     def __meta__(self):
         db_table = 'GroupUp_group'
-    
+
     def get_owner_group(self):
         all_entries = Group.objects.all()
         return all_entries
@@ -68,11 +68,12 @@ class Group(models.Model):
         return self.members.count()
 
 
+class GroupHasInterest(models.Model):
+    interest = models.ForeignKey(Interest, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 
 class GroupReport(models.Model):
-    reportID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique = True, editable=False)
-    reporter = models.ForeignKey(Profile, on_delete=models.SET_NULL)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
 
@@ -81,7 +82,7 @@ class GroupReport(models.Model):
 
 
 class MemberOfGroup(models.Model):
-    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    member = models.ForeignKey(Profile, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 
--- GroupUpProject/GroupUp/views.py
+++ GroupUpProject/GroupUp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from GroupUpProject.GroupUp.models import Profile, Group
+from .models import Profile, Group
 
 
 # Create your views here.
