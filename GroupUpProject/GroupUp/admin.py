4621b48aeb348e13d23a33b0a41ce21766e4005c
askis <askis@stud.ntnu.no>
2022-03-26 13:22:05 +0100

    Added GroupReport to admin page.

--- GroupUpProject/GroupUp/admin.py
+++ GroupUpProject/GroupUp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Group
+from .models import Profile, Group, GroupReport
 
 
-@admin.register(Profile, Group)
+@admin.register(Profile, Group, GroupReport)
 class UniversalAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
